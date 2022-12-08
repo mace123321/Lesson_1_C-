142cc343dba24a9199261af65d85168c3fa54176
Pantokha Alexander <ealex47@gmail.com>
2022-12-08 13:50:52 +0300

    Сделано первое дз

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿int NumFirst;
+int NumSec;
+NumFirst = Convert.ToInt32(Console.ReadLine());
+NumSec = Convert.ToInt32(Console.ReadLine());
+if (NumFirst > NumSec)
+{
+    Console.WriteLine(NumFirst + " Первое число больше второго");
+}
+else {
+    Console.WriteLine(NumSec + " Второе число больше второго");
+}
